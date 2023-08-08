d2607467d1bcdf646e684907777ea08b36571d97
petermugabo250 <mujohnpeter@gmail.com>
2023-08-08 17:28:29 +0200

    file added

---
+++ java.js
@@ -0,0 +1,66 @@
+let searchbtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("country-inp");
+
+searchbtn.addEventListener("click", () => {
+  let countryName =countryInp.value;
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+  console.log(finalURL);
+  fetch(finalURL).then((response) =>response.json())
+  .then((data)=>{
+    console.log(data[0]);
+    console.log(data[0].capital[0]);
+    console.log(data[0].flags.svg);
+    console.log(data[0].name.common); 
+    console.log(data[0].continents[0]);
+    console.log(Object.keys(data[0].currencies)[0]);
+    console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+    console.log(Object.values(data[0].languages).toString().split(",").join(","));
+    result.innerHTML = `
+    <img src="${data[0].flags.svg}" class="flag-img">
+    <h2>${data[0].name.common}</h2>
+    <div class="wrapper"> 
+      <div class="data-wrapper"> 
+       <h4><b>Capital:</b></h4>
+       ${data[0].capital[0]}
+      </div>
+    </div>
+      
+    <div class="wrapper"> 
+    <div class="data-wrapper"> 
+     <h4><b>Continent:</b></h4>
+     ${data[0].continents[0]}
+    </div>
+  </div>
+
+  <div class="wrapper"> 
+  <div class="data-wrapper"> 
+   <h4><b>Population:</b></h4>
+   ${data[0].population}
+  </div>
+</div>
+
+<div class="wrapper"> 
+  <div class="data-wrapper"> 
+   <h4><b>Currency</b>:</h4>
+   ${data[0].currencies[Object.keys(data[0].currencies)].name}-${Object.keys(data[0].currencies)}
+  </div>
+</div>
+   
+<div class="wrapper"> 
+<div class="data-wrapper"> 
+ <h4><b>Common:</b></h4>
+ ${Object.values(data[0].languages).toString().split(",").join(",")}
+</div>
+</div>
+    `;
+
+  }).catch(()=>{
+    if(countryName.lenght==0){
+        result.innerHTML='<h3>  Fill the field</h3>';
+
+    }
+    else{
+        result.innerHTML='<h3> Check if country is valid</h3>'
+    }
+  })
+});
